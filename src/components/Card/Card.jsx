0bed7b6bf392e64952d9271b19c23117d560ac33
germansoria05 <germanisoria05@gmail.com>
2023-10-31 20:59:51 -0300

    arreglo errores

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+
+
+const Card = () => {
+    return (
+       <h1>Ventas</h1>
+    );
+};
+
+export default Card;
--- src/components/CardWidget/CardWidget.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const CardWidget = () => {
-    return (
-       <h1>Ventas</h1>
-    );
-};
-
-export default CardWidget;
